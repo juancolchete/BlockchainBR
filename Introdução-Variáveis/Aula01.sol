0e9e1dd6a2727c726899456d1cb58f1c16983217
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-06 16:40:17 -0300

    Explicação SPDX e licença MIT

---
+++ Introdução-Variáveis/Aula01.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+// SPDX é um site que identifica as principais licenças do mundo de desenvolvimento de software, com o objetivo de facilitar a identificação destas
+// No caso deste repositório a licença utilizada é a MIT que permite o uso livremente do código contido neste repositório
+
+pragma solidity ^0.8.7;
