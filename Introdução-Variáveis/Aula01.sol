ff3d8c27c641403def3bd579bf4a76823623532f
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-06 17:10:10 -0300

    Adicionar exemplo de variáveis locais

--- Introdução-Variáveis/Aula01.sol
+++ Introdução-Variáveis/Aula01.sol
@@ -3,3 +3,11 @@
 // No caso deste repositório a licença utilizada é a MIT que permite o uso livremente do código contido neste repositório
 
 pragma solidity ^0.8.7;
+// Variáveis de estado − Variavéis em que os valores são permanentemente armazenadas na memória do contrato
+// Exemplo de variável de estado
+public uint soma;
+
+function setSoma(uint primeiroNumero, uint segundoNumero) public{
+    // variável de estado recebendo o valor da soma
+    soma = primeiroNumero + segundoNumero; 
+}
