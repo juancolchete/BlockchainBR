512d29cb6152111a20d575889c5dc11a86c12766
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-07 09:05:34 -0300

    Adicionar mais comentários

--- Introdução-Variáveis/Aula01.sol
+++ Introdução-Variáveis/Aula01.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: MIT
 // SPDX é um site que identifica as principais licenças do mundo de desenvolvimento de software, com o objetivo de facilitar a identificação destas
 // No caso deste repositório a licença utilizada é a MIT que permite o uso livremente do código contido neste repositório
+// SPDX-License-Identifier: MIT
 
+// Nesta linha é definido a versão do solidity que será utilizada ^0.8.7 versão 0.8.7 ou superior
 pragma solidity ^0.8.7;
 // Variáveis de estado − Variavéis em que os valores são permanentemente armazenadas na memória do contrato
 //contrato que representa a Aula01
